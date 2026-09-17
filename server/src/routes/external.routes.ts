1599595ce692b8a024f3edc067f254bdbf64ebb1
AaronPaulVillanueva <aaronvillanueva304@gmail.com>
2026-09-17 14:20:01 +0800

    Update

--- server/src/routes/external.routes.ts
+++ server/src/routes/external.routes.ts
@@ -1,13 +1,34 @@
 import { Router } from 'express';
 // @ts-ignore - IDE cache issue
-import { getTasks, postActivity, postComment, updateComment } from '../controllers/external.controller';
+import { 
+  getTasks, 
+  getTask,
+  createTask,
+  updateTask,
+  postActivity, 
+  postComment, 
+  updateComment,
+  deleteComment,
+  createSubtask,
+  updateSubtask
+} from '../controllers/external.controller';
 
 const router = Router();
 
-// These routes are protected by API Key (Bearer token or x-api-key header)
+// Task Core Operations
 router.get('/tasks', getTasks);
+router.post('/tasks', createTask);
+router.get('/tasks/:taskId', getTask);
+router.patch('/tasks/:taskId', updateTask);
+
+// Task Activity & Comments
 router.post('/activity', postActivity);
 router.post('/comment', postComment);
 router.put('/comment', updateComment);
+router.delete('/comment/:commentId', deleteComment);
+
+// Task Subtasks
+router.post('/tasks/:taskId/subtasks', createSubtask);
+router.patch('/tasks/:taskId/subtasks/:subtaskId', updateSubtask);
 
 export default router;
